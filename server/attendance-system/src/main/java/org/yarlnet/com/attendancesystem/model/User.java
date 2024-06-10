eec639b59ba94d04492b53652ba23f3e784fdd07
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2024-06-11 01:18:39 +0530

    #3.1, created User model

---
+++ server/attendance-system/src/main/java/org/yarlnet/com/attendancesystem/model/User.java
@@ -0,0 +1,18 @@
+package org.yarlnet.com.attendancesystem.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String username;
+    private String password;
+}
+
+
+
