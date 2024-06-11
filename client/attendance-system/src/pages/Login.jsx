b2998a05f7aa0730379d02f5f27894b49a94b914
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2024-06-12 05:13:53 +0530

    #5.5, updated Login component to handle authentication, display error messages using react-hot-toast, and redirect to home page on successful login

--- client/attendance-system/src/pages/Login.jsx
+++ client/attendance-system/src/pages/Login.jsx
@@ -1,21 +1,36 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { MdOutlineLightMode, MdDarkMode } from "react-icons/md";
+import { authenticateUser } from "../redux/authSlice";
+import { toast } from "react-hot-toast";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
   const [darkMode, setDarkMode] = useState(
-    localStorage.getItem("darkMode") === "true" || false
+    sessionStorage.getItem("darkMode") === "true" || true
   );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const authState = useSelector((state) => state.auth);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const resultAction = await dispatch(
+      authenticateUser({ username, password })
+    );
+    if (authenticateUser.fulfilled.match(resultAction)) {
+      navigate("/home");
+      toast.success("Login successful");
+    } else if (authenticateUser.rejected.match(resultAction)) {
+      toast.error(resultAction.payload || "Invalid username or password");
+    }
   };
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
-    localStorage.setItem("darkMode", !darkMode);
+    sessionStorage.setItem("darkMode", !darkMode);
   };
 
   return (
@@ -94,11 +109,27 @@ const Login = () => {
                 </div>
                 <button
                   type="submit"
-                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  className="py-2 px-4 flex justify-center items-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg max-w-md"
+                  disabled={authState.loading}
                 >
-                  Sign in
+                  {authState.loading ? (
+                    <>
+                      <svg
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        className="mr-2 animate-spin"
+                        viewBox="0 0 1792 1792"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M526 1394q0 53-37.5 90.5t-90.5 37.5q-52 0-90-38t-38-90q0-53 37.5-90.5t90.5-37.5 90.5 37.5 37.5 90.5zm498 206q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-704-704q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm1202 498q0 52-38 90t-90 38q-53 0-90.5-37.5t-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-964-996q0 66-47 113t-113 47-113-47-47-113 47-113 113-47 113 47 47 113zm1170 498q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-640-704q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm530 206q0 93-66 158.5t-158 65.5q-93 0-158.5-65.5t-65.5-158.5q0-92 65.5-158t158.5-66q92 0 158 66t66 158z"></path>
+                      </svg>
+                      {"Loading"}
+                    </>
+                  ) : (
+                    "Sign in"
+                  )}
                 </button>
-                {error && <p className="text-red-500">{error}</p>}
               </form>
             </div>
           </div>
