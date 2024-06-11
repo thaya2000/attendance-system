8cb32f1f9328f2bf5db96a846211730dbff2ed89
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2024-06-12 05:16:54 +0530

    #5.7, updated App component to handle routing based on authentication state, including redirect to home page if the user is already logged in

--- client/attendance-system/src/App.jsx
+++ client/attendance-system/src/App.jsx
@@ -1,13 +1,29 @@
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useSelector } from "react-redux";
 
 export default function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   return (
     <BrowserRouter>
+      <Toaster />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login"
+          element={isAuthenticated ? <Navigate to="/home" /> : <Login />}
+        />
+        <Route
+          path="/home"
+          element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/"
+          element={
+            isAuthenticated ? <Navigate to="/home" /> : <Navigate to="/login" />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
