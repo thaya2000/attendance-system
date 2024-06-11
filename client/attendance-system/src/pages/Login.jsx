5227bb0a37e52a1753247a671a1cfa94cc3a7a20
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2024-06-11 22:27:04 +0530

    #4.2, created Login page with input fields for username and password, and a login button

---
+++ client/attendance-system/src/pages/Login.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { MdOutlineLightMode, MdDarkMode } from "react-icons/md";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("darkMode") === "true" || false
+  );
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    localStorage.setItem("darkMode", !darkMode);
+  };
+
+  return (
+    <div className={darkMode ? "dark" : ""}>
+      <section className="bg-gray-50 dark:bg-gray-900 min-h-screen">
+        <div className="absolute bottom-4 right-4">
+          <button
+            onClick={toggleDarkMode}
+            className="focus:outline-none bg-gray-200 dark:bg-gray-700 p-2 rounded-full shadow-lg animate-blink-scale"
+          >
+            {darkMode ? (
+              <MdOutlineLightMode
+                size="2em"
+                className="text-gray-900 dark:text-white"
+              />
+            ) : (
+              <MdDarkMode
+                size="2em"
+                className="text-gray-900 dark:text-white"
+              />
+            )}
+          </button>
+        </div>
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <a
+            href="#"
+            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          >
+            <img
+              className="w-40 h-12 mr-2"
+              src="https://cdn.dorik.com/6118aaa4884caf0012fb362d/6118c560884caf0012fb364e/images/Gray-Logo_4z7ei6gc.svg"
+              alt="logo"
+            />
+          </a>
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Sign in to your account
+              </h1>
+              <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                <div>
+                  <label
+                    htmlFor="username"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your username
+                  </label>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter username"
+                    required
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Enter password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Sign in
+                </button>
+                {error && <p className="text-red-500">{error}</p>}
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Login;
